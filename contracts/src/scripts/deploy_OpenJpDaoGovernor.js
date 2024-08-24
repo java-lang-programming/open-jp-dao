5d73cfc26ba2d5adf2fd1fa7b73f8ef0978a2e1e
Masaya Suzuki <masayasuzuki@MasayanoMacBook-Pro.local>
2024-08-24 18:49:09 +0900

    vote create api.

--- contracts/src/scripts/deploy_OpenJpDaoGovernor.js
+++ contracts/src/scripts/deploy_OpenJpDaoGovernor.js
@@ -2,7 +2,7 @@
 const { ethers, upgrades } = require('hardhat');
 
 async function main () {
-  const VoteTokenAddress = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9"
+  const VoteTokenAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
   const OpenJpDaoGovernorFactory = await ethers.getContractFactory('OpenJpDaoGovernor');
   console.log('Deploying OpenJpDaoGovernor...');
   const OpenJpDaoGovernor = await OpenJpDaoGovernorFactory.deploy(VoteTokenAddress);
