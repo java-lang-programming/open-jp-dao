14e2deda42a9c7819ede4aaed1700cef2aaa1adf
Masaya Suzuki <masayasuzuki@MasayanoMacBook-Pro.local>
2024-10-01 11:41:09 +0900

    add votes routers.

--- backend/server/main.py
+++ backend/server/main.py
@@ -7,14 +7,12 @@ from decentralized.exceptions.invalid_chain_id import InvalidChainID
 from decentralized.usecase.tokens import Tokens
 from decentralized.usecase.nfts.create import Create
 #　こっちはproposal
-from decentralized.usecase.votes.vote_create import VoteCreate
 from decentralized.usecase.votes.vote_show import VoteShow
 from decentralized.usecase.votes.vote_cast import VoteCast
 from decentralized.responses.errors import Errors, ErrorCodes
 from decentralized.requests.nft import Nft
 from decentralized.requests.nonce import Nonce
 from decentralized.requests.verify import Verify
-from decentralized.requests.votes.votes_create import VoteCreateRequest
 from decentralized.requests.votes.votes_cast import VoteCastRequest
 from routers import votes
 import secrets
@@ -164,40 +162,6 @@ async def verify(verify: Verify):
     except Exception as e:
         print("エラーe")
         return Errors(code=ErrorCodes.INVALID_CHAIN_ID, message="chain_id error", detail=repr(e)).to_dict()
-
-#     # expiration_timeは1hってとこかな
-#     return {"status": "OK", "expiration_time": "aaaa"}
-
-# ta
-@app.post("/api/ethereum/{chain_id}/transfer")
-async def token_transfer(chain_id: str, voteCreate: VoteCreateRequest):
-  return None
-
-
-# 投票を作成する
-# proposal_create
-# curl -X POST -H "Content-Type: application/json" -d '{"description": "test", "token_address":"0x5FbDB2315678afecb367f032d93F642f64180aa3", "call_data_type": 1, "chain_id":8545, "from_address": "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266" }' http://localhost:8001/api/ethereum/8545/votes
-@app.post("/api/ethereum/{chain_id}/votes")
-async def vote_create(chain_id: str, voteCreate: VoteCreateRequest):
-    int_chain_id = 0
-    try:
-      int_chain_id = Chains.validate_chain_id(chain_id=chain_id)
-    except Exception as e: 
-      return Errors(code=ErrorCodes.INVALID_CHAIN_ID, message="chain_id error", detail=repr(e)).to_dict()
-
-    url = Chains.url_via_chain_id(chain_id=int_chain_id)
-
-    ethereum = Ethereum(url=url, chain_id=int_chain_id)
-    if not ethereum.is_connected():
-      return Errors(code=ErrorCodes.NOT_CONNECTED_ETHEREUM, message="イーサリアムに接続できませんでした", detail="接続先のステータスを確認してください").to_dict()
-
-    create = VoteCreate(ethereum=ethereum)
-    try:
-      result = create.execute(voteCreate=voteCreate)
-    except Exception as e:
-      return Errors(code=ErrorCodes.ERROR_VOTE_CREATE, message="vote create error", detail=repr(e)).to_dict()
-
-    return result
 
 # enum ProposalState {
 #         Pending,  0
