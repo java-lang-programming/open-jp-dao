e498eb0dfc1a18bbbdd72dcf602df131618ada44
Masaya Suzuki <masayasuzuki@MasayanoMacBook-Pro.local>
2025-01-31 10:55:58 +0900

    為替差益画面実装における修正

--- front/src/sample/sample/src/app/page_urls.js
+++ front/src/sample/sample/src/app/page_urls.js
@@ -1,3 +1,4 @@
 const FrontBaseUrl = "http://localhost:3010"
 
 export const ForeignExchangeGainIndex = `${FrontBaseUrl}/foreign_exchange_gain`
+export const DollaryenledgersUpdate = `${FrontBaseUrl}/dollaryenledgers/upload`
