d0b65649fbae1268595b1ddc14684011807040a6
Masaya Suzuki <masayasuzuki@MasayanoMacBook-Pro.local>
2025-03-19 19:54:42 +0900

    transaction_typesの作成

--- track/spec/requests/transaction_types_spec.rb
+++ track/spec/requests/transaction_types_spec.rb
@@ -2,6 +2,54 @@ require 'rails_helper'
 
 RSpec.describe "TransactionTypes", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    let(:addresses_eth) { create(:addresses_eth) }
+    let(:transaction_type1) { create(:transaction_type1, address: addresses_eth) }
+
+    context 'success' do
+      before do
+        # sigin処理
+        mock_apis_verify(body: {})
+        get apis_sessions_nonce_path
+        post apis_sessions_signin_path, params: { address: addresses_eth.address, kind: Address.kinds[:ethereum], chain_id: 1, message: "message", signature: "signature", domain: "aiueo.com" }
+      end
+
+      it "should not get transaction_types." do
+        get transaction_types_path
+        body = response.body
+        expect(response.status).to eq(200)
+        expect(body).to include '取引種別が存在しません。'
+      end
+
+      it "should get transaction_types." do
+        transaction_type1
+
+        get transaction_types_path
+        body = response.body
+        expect(response.status).to eq(200)
+        expect(body).to include "#{transaction_type1.name}"
+      end
+    end
+  end
+
+  describe "GET /new" do
+    let(:addresses_eth) { create(:addresses_eth) }
+    let(:transaction_type1) { create(:transaction_type1, address: addresses_eth) }
+
+    context 'success' do
+      before do
+        # sigin処理
+        mock_apis_verify(body: {})
+        get apis_sessions_nonce_path
+        post apis_sessions_signin_path, params: { address: addresses_eth.address, kind: Address.kinds[:ethereum], chain_id: 1, message: "message", signature: "signature", domain: "aiueo.com" }
+      end
+
+      it "should not get transaction_types." do
+        get new_transaction_type_path
+        body = response.body
+
+        expect(response.status).to eq(200)
+        expect(body).to include TransactionType::DEPOSIT_NAME
+      end
+    end
   end
 end
