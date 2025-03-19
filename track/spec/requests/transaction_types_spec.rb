1ce47230d790920a50914dd5d9bee9736ecc3909
Masaya Suzuki <masayasuzuki@MasayanoMacBook-Pro.local>
2025-03-19 20:06:41 +0900

    transaction_typesの作成

--- track/spec/requests/transaction_types_spec.rb
+++ track/spec/requests/transaction_types_spec.rb
@@ -52,4 +52,27 @@ RSpec.describe "TransactionTypes", type: :request do
       end
     end
   end
+
+  describe "post /create" do
+    let(:addresses_eth) { create(:addresses_eth) }
+    let(:transaction_type1) { create(:transaction_type1, address: addresses_eth) }
+
+    context 'success' do
+      before do
+        # sigin処理
+        mock_apis_verify(body: {})
+        get apis_sessions_nonce_path
+        post apis_sessions_signin_path, params: { address: addresses_eth.address, kind: Address.kinds[:ethereum], chain_id: 1, message: "message", signature: "signature", domain: "aiueo.com" }
+      end
+
+      it "should create transaction_types." do
+        post transaction_types_path, params: { transaction_type: { name: "test", kind: 1 } }
+        body = response.body
+
+        expect(response.status).to eq(302)
+        expect(addresses_eth.transaction_types.count).to eq(1)
+      end
+    end
+  end
+
 end
