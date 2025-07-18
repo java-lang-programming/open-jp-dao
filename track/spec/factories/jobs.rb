2ef113d93d229796da203cda333c6fd0abdbd86f
Masaya Suzuki <masayasuzuki@MasayanoMacBook-Pro.local>
2025-07-18 09:16:38 +0900

    画面改善

--- track/spec/factories/jobs.rb
+++ track/spec/factories/jobs.rb
@@ -1,19 +1,19 @@
 FactoryBot.define do
   factory :job_1, class: Job do
     id { Job::DOLLAR_YENS_CSV_IMPORT }
-    name { "ドル円csvimport" }
+    name { "ドル円csvインポート" }
     summary { "ドル円のcsvをimportします" }
   end
 
   factory :job_2, class: Job do
     id { Job::DOLLAR_YENS_TRANSACTIONS_CSV_IMPORT }
-    name { "ドル円取引csvimport" }
+    name { "ドル円取引csvインポート" }
     summary { "ドル円取引のcsvをimportします" }
   end
 
   factory :job_3, class: Job do
     id { Job::LEDGER_CSV_IMPORT }
-    name { "仕訳帳csvimport" }
+    name { "仕訳帳csvインポート" }
     summary { "仕訳帳のcsvをimportします" }
   end
 end
