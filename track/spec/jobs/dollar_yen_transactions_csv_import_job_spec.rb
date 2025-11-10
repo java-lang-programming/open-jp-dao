29e306cf36e1ca90b11c7ebf3d1b0f78211a16b7
Masaya Suzuki <masayasuzuki@MasayanoMacBook-Pro.local>
2025-11-10 20:25:34 +0900

    fix duplicated keys.

--- track/spec/jobs/dollar_yen_transactions_csv_import_job_spec.rb
+++ track/spec/jobs/dollar_yen_transactions_csv_import_job_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe DollarYenTransactionsCsvImportJob, type: :job do
+  let(:addresses_eth) { create(:addresses_eth) }
+  let(:job_2) { create(:job_2) }
+  let(:import_file) { create(:import_file, job: job_2, address: addresses_eth) }
+
   # 実行結果の確認
   describe 'perform' do
-    let(:addresses_eth) { create(:addresses_eth) }
-    let(:job_2) { create(:job_2) }
-    let(:import_file) { create(:import_file, job: job_2, address: addresses_eth) }
-
     it 'should be status complete when job is success.' do
       DollarYenTransactionsCsvImportJob.perform_now(import_file_id: import_file.id)
       result_import_file = ImportFile.find(import_file.id)
@@ -16,11 +16,8 @@ RSpec.describe DollarYenTransactionsCsvImportJob, type: :job do
   end
 
   describe 'csv pattern' do
-    let(:addresses_eth) { create(:addresses_eth) }
-    let(:job_2) { create(:job_2) }
     let(:transaction_type1) { create(:transaction_type1, address: addresses_eth) }
     let(:transaction_type2) { create(:transaction_type2, address: addresses_eth) }
-    let(:import_file) { create(:import_file, job: job_2, address: addresses_eth) }
     let(:import_file_2) { create(:import_file, job: job_2, address: addresses_eth) }
     let(:deposit_series_1_csv_path) { "#{Rails.root}/spec/files/uploads/dollar_yen_transaction_deposit_csv/deposit_series_1.csv" }
     let(:deposit_series_2_csv_path) { "#{Rails.root}/spec/files/uploads/dollar_yen_transaction_deposit_csv/deposit_series_2.csv" }
@@ -46,6 +43,26 @@ RSpec.describe DollarYenTransactionsCsvImportJob, type: :job do
       import_file_2.file.purge
 
       # TODO exportして、期待していいるデータかを比較する(excelで作る)
+
+      FileUtils.rm_rf(ActiveStorage::Blob.service.root)
+    end
+  end
+
+  describe 'duplicate key' do
+    let(:transaction_type1) { create(:transaction_type1, address: addresses_eth) }
+    let(:duplicate_key_csv_path) { "#{Rails.root}/spec/files/uploads/dollar_yen_transaction_deposit_csv/duplicate_key.csv" }
+
+    it 'should be status complete when job is success.' do
+      transaction_type1
+
+      file = File.new(duplicate_key_csv_path)
+      import_file.file.attach(file)
+      import_file.save
+
+      DollarYenTransactionsCsvImportJob.perform_now(import_file_id: import_file.id)
+
+      errors = import_file.import_file_errors.first.fetch_errors
+      expect(errors).to eq([ { row: -1, col: -1, attribute: "例外処理", value: "import", message: "SQLite3::ConstraintException: UNIQUE constraint failed: dollar_yen_transactions.transaction_type_id, dollar_yen_transactions.date" } ])
 
       FileUtils.rm_rf(ActiveStorage::Blob.service.root)
     end
