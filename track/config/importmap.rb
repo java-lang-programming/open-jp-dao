f6d0b5b5b5b0136b7931f30296397ce36ddf584c
Masaya Suzuki <masayasuzuki@MasayanoMacBook-Pro.local>
2025-11-17 09:26:56 +0900

    update csv upload processing.

--- track/config/importmap.rb
+++ track/config/importmap.rb
@@ -3,3 +3,5 @@
 pin "application", preload: true
 
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin_all_from "app/javascript/views", under: "views"
+pin_all_from "app/javascript/shared", under: "shared"
