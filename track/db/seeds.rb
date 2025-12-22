f9cafe3cf9ece48e17414dde67872fb0ae985b11
java-lang-programming <m.suzuki.gm1@gmail.com>
2025-12-22 09:33:15 +0900

    Feature/make rails prod (#105)
    * make rails production docker.
    
    * backend prod docker.
    
    * update db/seeds.rb
    
    * update Gemfile.lock.
    
    * create db/seeds/production.rb

--- track/db/seeds.rb
+++ track/db/seeds.rb
@@ -1,2 +1,7 @@
-load(Rails.root.join("db", "seeds", "#{Rails.env.downcase}.rb"))
-load(Rails.root.join("db", "seeds", "#{Rails.env.downcase}_v2.rb"))
+current_env = Rails.env.downcase
+if current_env == development
+  load(Rails.root.join("db", "seeds", "#{Rails.env.downcase}.rb"))
+  load(Rails.root.join("db", "seeds", "#{Rails.env.downcase}_v2.rb"))
+else
+  load(Rails.root.join("db", "seeds", "#{Rails.env.downcase}.rb"))
+end
