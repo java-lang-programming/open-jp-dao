e18028a77b7b2283af66c95f0010269f20e0579f
java-lang-programming <m.suzuki.gm1@gmail.com>
2026-01-18 20:00:49 +0900

    update gitHubAction (#112)
    Co-authored-by: Masaya Suzuki <masayasuzuki@MasayanoMacBook-Pro.local>

--- track/spec/spec_helper.rb
+++ track/spec/spec_helper.rb
@@ -19,8 +19,7 @@ require 'simplecov'
 
 SimpleCov.start 'rails'
 
-# 最終的に85まであげること
-SimpleCov.minimum_coverage 85
+SimpleCov.minimum_coverage 90
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
