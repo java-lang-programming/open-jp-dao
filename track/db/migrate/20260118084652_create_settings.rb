3593ce16e113f98d5dfb540770c7326cd475fc20
Masaya Suzuki <masayasuzuki@MasayanoMacBook-Pro.local>
2026-03-15 00:11:09 +0900

    言語

--- track/db/migrate/20260118084652_create_settings.rb
+++ track/db/migrate/20260118084652_create_settings.rb
@@ -2,8 +2,8 @@ class CreateSettings < ActiveRecord::Migration[8.1]
   def change
     create_table :settings do |t|
       t.references :address, null: false, foreign_key: true, comment: "address_idに紐づく"
-      t.integer :default_year, null: false
-      t.string :language, null: false, default: 'ja'
+      t.integer :default_year, null: false, comment: "デフォルト年度"
+      t.string :language, null: false, default: 'ja', comment: "言語"
 
       t.timestamps
     end
